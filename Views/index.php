6ea4a331acbaa50619f9c57bf544fe2e2f47049e
Simon Pacheco <Simon@Simons-MacBook-Pro-2.local>
2015-06-14 02:10:04 -0400

    Need to fix sign in bug

--- Views/index.php
+++ Views/index.php
@@ -1,5 +1,7 @@
 <?php
 	session_start();
+
+	//print_r($_SESSION);
 ?>
 
 <!DOCTYPE html>
@@ -10,22 +12,41 @@
 <body>
 
 <?php if (!isset($_SESSION['user_id'])): ?>
-	<h1>Sign In to Eventhut</h1>
-	<form action="../Scripts/authenticate_user.php" method="post">
-		First Name:<br>
-		<input type="text" name="fName">
-		<br>
-		Last Name:<br>
-		<input type="text" name="lName">
-		<br>
-		Username:<br>
-		<input type="text" name="username">
-		<br>
-		Password:<br>
-		<input type="password" name="password">
-		<br>
-		<input type="submit" value="Sign In">
-	</form>
+
+	<div>
+		<h1>Sign In to Eventhut</h1>
+		<form action="../Scripts/login.php" method="post">
+			Username:<br>
+			<input type="text" name="username">
+			<br>
+			Password:<br>
+			<input type="password" name="password">
+			<br>
+			<input type="submit" value="Sign In">
+		</form>
+	</div>
+
+	<div>
+		<h2>Sign Up For Eventhut!</h2>
+		<form action="../Scripts/signup.php" method="post">
+			First Name:<br>
+			<input type="text" name="fName">
+			<br>
+			Last Name:<br>
+			<input type="text" name="lName">
+			<br>
+			Username:<br>
+			<input type="text" name="username">
+			<br>
+			Password:<br>
+			<input type="password" name="password">
+			<br>
+			<input type="submit" value="Sign Up">
+		</form>
+	</div>
+
+<?php else: ?>
+
 <?php endif; ?>
 
 </body>
